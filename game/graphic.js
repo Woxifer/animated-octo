b1083611ee83934ea8442ba54bfdd9abdabd8bd2
Pierre Sadki <pierre.sadki@epita.fr>
2021-09-22 20:27:38 +0200

    Allow to see the enemy

--- game/graphic.js
+++ game/graphic.js
@@ -28,6 +28,9 @@ function init()
     player1 = new Player("player1", 0xffff00, new THREE.Vector2(50, 0), 0);
     scene.add(player1.graphic);
 
+    player2 = new Player("player2", 0x00ffff, new THREE.Vector2(0, 50), 0);
+    scene.add(player2.graphic);
+
     light1 = new Light("sun", 0xffffff, "0,0,340");
     scene.add(light1);
 }
@@ -48,7 +51,6 @@ function Ground(color, size_x, size_y, nb_tile)
         for (y = minY; y <= maxY; y = y+sizeOfTileY){
 
             color = colors[Math.floor(Math.random()*colors.length)];
-       
             if (0x000000 != color)
             {
                 tmpGround = new THREE.Mesh(
